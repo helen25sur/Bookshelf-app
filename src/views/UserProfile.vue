906e138cfdebf48424896d99f38d9cc84933a846
Elena Surilova <elen21surilova@gmail.com>
2023-03-22 00:33:57 +0200

    add form for a user profile

--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -2,12 +2,9 @@
 <template>
   <v-container>
     <h1>User Profile</h1>
-    <v-card
-      theme="dark" class="mx-auto" max-width="70%" variant="outlined">
+    <v-card theme="dark" class="mx-auto" max-width="70%" variant="outlined">
       <div class="d-flex flex-no-wrap justify-start pa-6">
-        <v-avatar class="ma-3" size="225" rounded="0">
-          <v-img src="./img/Female_avatar.png">
-          </v-img>
+        <v-avatar image="./img/Female_avatar.png" class="ma-3" size="225" rounded="0">
         </v-avatar>
         <div class="pl-4">
           <v-card-title class="text-h4 text-purple-darken-1 mb-2">
@@ -15,27 +12,59 @@
           </v-card-title>
           <v-card-subtitle class="text-h5 mb-2">About User</v-card-subtitle>
           <v-card-text>
-          <p class="text--primary text-body-1 mb-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore iste in corrupti qui, sunt blanditiis mollitia minima sed. Quam dolore est enim quis cupiditate, molestias sunt voluptatem explicabo ipsam provident?</p>
-          <p class="text--primary text-body-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore iste in corrupti qui, sunt blanditiis mollitia minima sed. Quam dolore est enim quis cupiditate, molestias sunt voluptatem explicabo ipsam provident?</p>
+            <p class="text--primary text-body-1 mb-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
+              iste in corrupti qui, sunt blanditiis mollitia minima sed. Quam dolore est enim quis cupiditate, molestias
+              sunt voluptatem explicabo ipsam provident?</p>
+            <p class="text--primary text-body-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore iste in
+              corrupti qui, sunt blanditiis mollitia minima sed. Quam dolore est enim quis cupiditate, molestias sunt
+              voluptatem explicabo ipsam provident?</p>
 
           </v-card-text>
-          <!-- <v-card-actions>
-            <v-btn class="ms-2" variant="outlined" size="small">
-
+          <v-card-actions>
+            <v-btn class="ms-2" variant="outlined" color="primary">
+              Edit Profile
             </v-btn>
-          </v-card-actions> -->
+          </v-card-actions>
         </div>
       </div>
+    </v-card>
+    <v-card variant="outlined" class="mx-auto mt-10 pa-5" max-width="70%">
+      <form>
+        <v-text-field v-model="name" prepend-icon="mdi-account-edit" label="Name"></v-text-field>
+
+        <v-text-field v-model="email" prepend-icon="mdi-email" label="E-mail"></v-text-field>
+
+        <v-file-input label="Avatar image" variant="filled" prepend-icon="mdi-camera"></v-file-input>
+
+        <v-textarea prepend-icon="mdi-pen" label="Tell about yourself"></v-textarea>
+        <v-card-actions class="d-flex justify-center">
+          <v-btn class="me-4" color="primary" variant="outlined" type="submit">
+          submit
+          </v-btn>
+
+          <v-btn color="warning" variant="outlined">
+            clear
+          </v-btn>
+        </v-card-actions>
+      </form>
     </v-card>
   </v-container>
 </template>
 
 <script>
-// import AvatarImg from '@/assets/Female_avatar.png';
 
 export default {
-  components: {
-    // AvatarImg,
+  data() {
+    return {
+      name: '',
+      email: '',
+      aboutUserDescr: '',
+      rules: {
+        required: (value) => !!value || 'Required field',
+        emailRule: (v) => /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(v) || 'Incorrect email',
+        min: (v) => v.length >= 20 || 'Minimum 20 characters',
+      },
+    };
   },
 };
 </script>
