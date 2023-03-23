234b4f446b8a8ffab9a88ee52fa0213292d20c2b
Elena Surilova <elen21surilova@gmail.com>
2023-03-23 03:23:13 +0200

    add functions to edit user profile

--- src/store/user.js
+++ src/store/user.js
@@ -3,9 +3,19 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signOut,
+  updateProfile,
+  updateEmail,
 } from 'firebase/auth';
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  update,
+  onValue,
+} from 'firebase/database';
 
-import { getDatabase, ref, set } from 'firebase/database';
+import { getStorage, ref as refStorage, uploadBytes } from 'firebase/storage';
 
 function writeUserData(userId, email) {
   const db = getDatabase();
@@ -15,6 +25,48 @@ function writeUserData(userId, email) {
     },
     'wishlist-books': [],
     'readlist-books': [],
+  });
+}
+
+function updateUserData(userId, data) {
+  const db = getDatabase();
+  const userProfileRef = ref(db, `users/${userId}/profile`);
+  try {
+    onValue(userProfileRef, (snapshot) => {
+      const dataBD = snapshot.val();
+      if (dataBD) {
+        const updates = {};
+        updates[`/users/${userId}/profile`] = data;
+        update(ref(db), updates);
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// eslint-disable-next-line consistent-return
+async function getUserDate(userId) {
+  const db = getDatabase();
+  const userProfileRef = ref(db, `users/${userId}/profile`);
+  try {
+    const snapshot = await get(userProfileRef);
+    if (snapshot.exists()) {
+      return snapshot.val();
+    }
+    return null;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function uploadImage(userId, file) {
+  const storage = getStorage();
+  const storageRef = refStorage(storage, `avatars-${userId}`);
+
+  // 'file' comes from the Blob or File API
+  uploadBytes(storageRef, file).then((snapshot) => {
+    console.log('Uploaded a file!', snapshot);
   });
 }
 
@@ -75,6 +127,39 @@ export default {
       } else {
         commit('UNSET_USER');
       }
+    },
+    GET_USER_PROFILE() {
+      const auth = getAuth();
+      const user = auth.currentUser;
+
+      return getUserDate(user.uid);
+      // const {
+      //   displayName, email, photoURL,
+      // } = user;
+      // console.log(displayName, email, photoURL);
+      // return { displayName, email, photoURL };
+    },
+    UPDATE_USER_PROFILE({ commit }, payload) {
+      commit('SET_PROCESSING', true);
+      commit('CLEAR_ERROR');
+      const auth = getAuth();
+      updateProfile(auth.currentUser, {
+        displayName: payload.displayName,
+        photoURL: payload.photoURL,
+      }).then(() => {
+        console.log('displayName & photoURL updated!', auth.currentUser);
+      }).catch((error) => {
+        console.log(error);
+      });
+
+      updateEmail(auth.currentUser, payload.email).then(() => {
+        console.log('Email updated!', auth.currentUser);
+      }).catch((error) => {
+        console.log(error);
+      });
+
+      updateUserData(auth.currentUser.uid, payload);
+      uploadImage(auth.currentUser.uid, payload.photoURL);
     },
   },
 };
--- src/views/UserProfile.vue
+++ src/views/UserProfile.vue
@@ -3,21 +3,20 @@
   <v-container>
     <h1>User Profile</h1>
     <v-card theme="dark" class="mx-auto" max-width="70%" variant="outlined">
-      <div class="d-flex flex-no-wrap justify-start pa-6">
-        <v-avatar image="./img/Female_avatar.png" class="ma-3" size="225" rounded="0">
+      <div v-if="!isLoading" class="d-flex flex-no-wrap justify-start pa-6">
+        <v-avatar class="ma-3" size="225" rounded="0">
+        <img :src="photoURL" :alt="displayName" width="225">
         </v-avatar>
         <div class="pl-4">
           <v-card-title class="text-h4 text-purple-darken-1 mb-2">
-            UserName
+            {{ displayName }}
           </v-card-title>
           <v-card-subtitle class="text-h5 mb-2">About User</v-card-subtitle>
           <v-card-text>
-            <p class="text--primary text-body-1 mb-2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore
-              iste in corrupti qui, sunt blanditiis mollitia minima sed. Quam dolore est enim quis cupiditate, molestias
-              sunt voluptatem explicabo ipsam provident?</p>
-            <p class="text--primary text-body-1">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore iste in
-              corrupti qui, sunt blanditiis mollitia minima sed. Quam dolore est enim quis cupiditate, molestias sunt
-              voluptatem explicabo ipsam provident?</p>
+            <a :href="'mailto:' + email" class="text--primary text-body-1">{{ email }}</a>
+            <p class="text--primary text-body-1 mt-3 mb-2">
+              {{ aboutUserDescr }}
+            </p>
 
           </v-card-text>
           <v-card-actions>
@@ -27,19 +26,21 @@
           </v-card-actions>
         </div>
       </div>
+      <LoaderComponent v-else-if="isLoading" class="d-flex my-6 mx-auto" size="65" width="6"></LoaderComponent>
     </v-card>
     <v-card v-if="isEditForm" variant="outlined" class="mx-auto mt-10 pa-5" max-width="70%">
-      <form>
-        <v-text-field v-model="name" prepend-icon="mdi-account-edit" label="Name"></v-text-field>
+      <form @submit.prevent="saveUserChanges">
+        <v-text-field v-model="displayName" prepend-icon="mdi-account-edit" label="Name" :value="displayName"></v-text-field>
 
-        <v-text-field v-model="email" prepend-icon="mdi-email" label="E-mail"></v-text-field>
+        <v-text-field v-model="email" prepend-icon="mdi-email" label="E-mail" :value="email"></v-text-field>
 
-        <v-file-input label="Avatar image" variant="filled" prepend-icon="mdi-camera"></v-file-input>
+        <v-file-input label="Avatar image" variant="filled" prepend-icon="mdi-camera" :filename="photoURL" @change="onImagePicker" accept="image/*"></v-file-input>
 
-        <v-textarea prepend-icon="mdi-pen" label="Tell about yourself"></v-textarea>
+        <v-textarea v-model="aboutUserDescr" prepend-icon="mdi-pen" label="Tell about yourself"
+          :value="aboutUserDescr"></v-textarea>
         <v-card-actions class="d-flex justify-center">
           <v-btn class="me-4" color="primary" variant="outlined" type="submit">
-          submit
+            submit
           </v-btn>
 
           <v-btn @click="isEditForm = false" color="warning" variant="outlined">
@@ -52,20 +53,64 @@
 </template>
 
 <script>
+import LoaderComponent from '@/components/LoaderComponent.vue';
 
 export default {
+  components: {
+    LoaderComponent,
+  },
+  mounted() {
+    this.getUserDate();
+  },
   data() {
     return {
-      name: '',
-      email: '',
-      aboutUserDescr: '',
+      displayName: 'UserName',
+      email: 'username@user.com',
+      photoURL: './img/Female_avatar.png',
+      aboutUserDescr: 'Write about yourself',
       isEditForm: false,
       rules: {
         required: (value) => !!value || 'Required field',
         emailRule: (v) => /^([a-z0-9_-]+\.)*[a-z0-9_-]+@[a-z0-9_-]+(\.[a-z0-9_-]+)*\.[a-z]{2,6}$/.test(v) || 'Incorrect email',
         min: (v) => v.length >= 20 || 'Minimum 20 characters',
       },
+      isLoading: true,
     };
+  },
+  methods: {
+    saveUserChanges() {
+      this.isEditForm = false;
+      this.$store.dispatch('UPDATE_USER_PROFILE', {
+        displayName: this.displayName,
+        email: this.email,
+        photoURL: this.photoURL,
+        aboutUser: this.aboutUserDescr,
+      });
+    },
+    async getUserDate() {
+      const {
+        displayName,
+        email,
+        photoURL,
+        aboutUser,
+      } = await this.$store.dispatch('GET_USER_PROFILE');
+      this.displayName = displayName !== null ? displayName : 'UserName';
+      this.email = email !== null ? email : 'username@user.com';
+      this.photoURL = photoURL !== null ? photoURL : './img/Female_avatar.png';
+      this.aboutUserDescr = aboutUser !== null ? aboutUser : 'Write about yourself';
+      this.isLoading = false;
+    },
+    onImagePicker(e) {
+      const { files } = e.target;
+
+      const fr = new FileReader();
+      fr.readAsDataURL(files[0]);
+      fr.addEventListener('load', () => {
+        const imageUrl = fr.result;
+        // eslint-disable-next-line prefer-destructuring
+        this.photoURL = imageUrl;
+      });
+    },
   },
 };
 </script>
