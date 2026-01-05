c670476a6d0ebda6b22743ccd86c1612e7621177
Olena Surilova <elen21surilova@gmail.com>
2026-01-05 20:26:41 +0100

    fix: edit vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,15 @@
+const { fileURLToPath, URL } = require('node:url');
 const { defineConfig } = require('@vue/cli-service');
+
 require('dotenv').config();
 
 module.exports = defineConfig({
   transpileDependencies: true,
   // publicPath: process.env.NODE_ENV === 'production' ? '/Bookshelf-app/' : '/',
   publicPath: '/Bookshelf-app/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
